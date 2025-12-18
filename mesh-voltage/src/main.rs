f5dcdf1e7ab38252d763e3c5be03cc9b70cbf930
v <daderiovictorfelipe@gmail.com>
2025-12-17 23:58:51 -0300

    Implementado sensor de temperatura do processador

--- mesh-voltage/src/main.rs
+++ mesh-voltage/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::format;
+
+use mesh_core::{WaybarPayload, print_waybar};
+
+fn read_cpu_temp_c() -> std::io::Result<f32> {
+    let raw_temp = std::fs::read_to_string("/sys/class/hwmon/hwmon1/temp1_input")?;
+    let processed_temp: f32 = raw_temp.trim().parse().unwrap_or(0.0);
+    Ok(processed_temp / 1000.0_f32)
 }
+
+fn main(){
+
+    let temperature = read_cpu_temp_c().unwrap_or(0.0_f32);
+
+    let string: String = format!("{temperature}ºC 12.0V");
+
+
+    let payload = WaybarPayload {
+        text: string,
+        description: Some("Usando 100% voltagem".into()),
+        graphics: None
+    };
+
+    print_waybar(&payload);
+}
+
+
+/*
+for d in /sys/class/hwmon/hwmon*; do
+    echo "== $d =="
+    cat "$d/name"
+done 
+*/
--- target/debug/mesh-voltage.d
+++ target/debug/mesh-voltage.d
@@ -1,1 +1,1 @@
-/home/v/mesh-bar-modules/target/debug/mesh-voltage: /home/v/mesh-bar-modules/mesh-voltage/src/main.rs
+/home/v/mesh-bar-modules/target/debug/mesh-voltage: /home/v/mesh-bar-modules/mesh-core/src/lib.rs /home/v/mesh-bar-modules/mesh-voltage/src/main.rs
