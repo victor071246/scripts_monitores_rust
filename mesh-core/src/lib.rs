e510984e811ce69fa725c9c0b6789fd5e7dae271
v <daderiovictorfelipe@gmail.com>
2025-12-19 23:55:08 -0300

    Feita a função que converte o número pra string de malha

--- mesh-core/src/lib.rs
+++ mesh-core/src/lib.rs
@@ -1,8 +1,17 @@
 use serde::Serialize;
 use serde_json;
 
+fn percent_to_bars(number_float: &f32) -> String {
+    let number_rounded = (number_float / 10.0).round() * 10.0;
+    let number_int: i32 = number_rounded as i32;
+    let max_tiles: i32 = 10;
+    let tiles: i32 = number_int / max_tiles;
+    let mesh = max_tiles - tiles;
 
-
+    let filled_tile = "󰝤".repeat(tiles as usize);         
+    let mesh_tile = "󱗼".repeat(mesh as usize);
+    format!("{filled_tile}{mesh_tile}")
+}
 
 #[derive(Serialize)]
 pub struct WaybarPayload {
